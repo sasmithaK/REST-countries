5b74953ad053fdb37191642c080849a5cc731d85
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-04-17 21:20:20 +0530

    chore: project initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return(
+    <div>
+      <h1>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  )
+}
+
+export default App
