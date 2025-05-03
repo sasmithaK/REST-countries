54dbcefbd25648a77cd873455645917003659e0c
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-05-03 23:23:23 +0530

    feat: country filter by basic languages

---
+++ src/components/Filters.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const regions = ["Africa","Americas","Asia","Europe","Oceania"];
+const languages = ["English","Spanish","French","Arabic","Chinese"];
+
+export default function Filters({ region, onRegion, language, onLanguage }) {
+  return (
+    <div className="flex space-x-4">
+      <select value={region} onChange={e => onRegion(e.target.value)} className="p-2 border rounded">
+        <option value="">All Regions</option>
+        {regions.map(r => <option key={r} value={r}>{r}</option>)}
+      </select>
+      <select value={language} onChange={e => onLanguage(e.target.value)} className="p-2 border rounded">
+        <option value="">All Languages</option>
+        {languages.map(l => <option key={l} value={l}>{l}</option>)}
+      </select>
+    </div>
+  );
+}
