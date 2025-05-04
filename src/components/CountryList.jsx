24ec272daa92dfee1e87dea2abc543cb0ef54ad8
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-05-04 15:19:17 +0530

    chore: styles

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -4,7 +4,7 @@ import CountryCard from "./CountryCard";
 export default function CountryList({ countries }) {
   if (!countries.length) return <p>No countries found.</p>;
   return (
-    <div className="row g-4">
+    <div className="row g-3">
       {countries.map(c => (
         <div key={c.cca3} className="col-12 col-sm-6 col-lg-3">
           <CountryCard country={c} />
